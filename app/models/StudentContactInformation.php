1e6d2f75739aaab59cfaa8e1275bd54770e7b8dc
BQumobile <bqu.mobile.dev@gmail.com>
2015-07-27 11:39:37 +0530

    address issue fixed

--- app/models/StudentContactInformation.php
+++ app/models/StudentContactInformation.php
@@ -6,7 +6,7 @@ class StudentContactInformation extends \Eloquent {
     protected $table = 'student_contact_informations';
 
     public function scopeLastRecordBySAN($query,$san) {
-        return $query->where('san','=',$san)->orderBy('id', 'desc')->first();
+        return $query->where('san','=',$san)->where('student_contact_information_type','=','2')->orderBy('id', 'desc')->first();
     }
     public function scopeLastUKRecordBySAN($query,$san) {
         return $query->where('san','=',$san)->where('student_contact_information_type','=','1')->orderBy('id', 'desc')->first();
